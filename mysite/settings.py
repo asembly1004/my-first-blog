cfce42ab52bf313934bc798475c0ce73810474ed
admin <asembly1005@gmail.com>
2017-03-15 09:28:28 +0900

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0jaf^j_7m=0$zukq3)gk)g&5b0h6qf00-t7$qz)#weaf895m-h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asembly1004.pythonanywhere.com']
 
 
 # Application definition
